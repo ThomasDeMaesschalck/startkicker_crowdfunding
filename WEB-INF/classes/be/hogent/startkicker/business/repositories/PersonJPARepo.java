bbb2446639327920e5901ed5d2a5a17d725c216a
Thomas De Maesschalck <thomas.dm@dvhardware.net>
2021-05-06 22:22:06 +0200

    Refactored

--- WEB-INF/classes/be/hogent/startkicker/business/repositories/PersonJPARepo.java
+++ WEB-INF/classes/be/hogent/startkicker/business/repositories/PersonJPARepo.java
@@ -18,7 +18,7 @@ public class PersonJPARepo implements IPersonRepo {
 	 */
 	private static final long serialVersionUID = 1L;
 
-	private static final String NAME_PERSISTENCEUNIT = "DemoLayered2.2";
+	private static final String NAME_PERSISTENCEUNIT = "startkicker";
 
 	private EntityManagerFactory emf = null;
 	private EntityManager em = null;
